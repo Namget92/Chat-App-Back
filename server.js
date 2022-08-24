eb2be7803c8344b2328c528972a695aa7c0f4de6
Namget92 <timtegman@gmail.com>
2022-08-24 17:59:55 +0200

    ..

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "https://tgt-chat-app-front.herokuapp.com",
+    origin: "*",
     methods: ["GET", "POST"],
-    credentials: true,
-    extraHeaders: {
-      "Access-Control-Allow-Credentials": "omit",
-    },
   },
 });
 const fs = require("fs");
