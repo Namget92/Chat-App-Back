72a6b66a88b32b73f5c75cb67722acd56dc1a21b
Namget92 <timtegman@gmail.com>
2022-08-25 09:08:10 +0200

    mind your own buissness

--- models/auth.model.js
+++ models/auth.model.js
@@ -9,6 +9,7 @@ async function authRegister(user) {
     user.email,
     user.password,
   ]);
+  console.log(result.rows);
   return result.rows;
 }
 
@@ -17,6 +18,7 @@ async function authRegister(user) {
 async function authLogin(email) {
   const sql = "SELECT * FROM users WHERE username = $1";
   const result = await userPGDB.query(sql, [email]);
+  console.log(result.rows);
   return result.rows;
 }
 
@@ -30,12 +32,14 @@ async function authChat(sender, receiver, text, type, date) {
     type,
     date,
   ]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function updateSocketId(socketId, user) {
   const sql = "UPDATE users SET socketId = $1 WHERE username = $1";
   const result = await userPGDB.query(sql, [socketId, user]);
+  console.log(result.rows);
   return result.rows;
 }
 
@@ -43,42 +47,49 @@ async function authChats() {
   const type = "group";
   const sql = "SELECT DISTINCT receiver FROM chat WHERE type = $1";
   const result = await chatPGDB.query(sql, [type]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function getChat(receiver) {
   const sql = "SELECT * FROM chat WHERE receiver = $1";
   const result = await chatPGDB.query(sql, [receiver]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function getDMChat(sender, receiver) {
   const sql = "SELECT * FROM chat WHERE receiver = $1 OR receiver = $1";
   const result = await chatPGDB.query(sql, [sender, receiver]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function deleteChat(receiver) {
   const sql = "DELETE FROM chat WHERE receiver = $1";
   const result = await chatPGDB.query(sql, [receiver]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function authUsers(username) {
   const sql = "SELECT username FROM users WHERE username != $1";
   const result = await userPGDB.query(sql, [username]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function getSocketReceiver(username) {
   const sql = "SELECT socketId FROM users WHERE username = $1";
   const result = await userPGDB.query(sql, [username]);
+  console.log(result.rows);
   return result.rows;
 }
 
 async function getSocketSender(username) {
   const sql = "SELECT socketId FROM users WHERE username = $1";
   const result = await userPGDB.query(sql, [username]);
+  console.log(result.rows);
   return result.rows;
 }
 module.exports = {
